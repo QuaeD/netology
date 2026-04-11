d5c3c1b62d34574cd3d66ad1983678523eb191f5
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-04-11 17:50:11 +0300

    add HW #2

---
+++ 1.2/02/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <windows.h>
+
+#include <iostream>
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	std::string word{};
+	std::cout << "Введите слово:" << std::endl;
+	std::cin >> word;
+	std::cout << "Вы ввели:\n" << word << std::endl;
+
+	return EXIT_SUCCESS;
+}
