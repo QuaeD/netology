cc86ffdfe622a7a9ed5f4247cf35ad3bc938c277
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-04-17 12:14:47 +0300

    add hw #3

---
+++ 1.3/01/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	std::cout << std::boolalpha;
+	std::cout << "Оператор: ||\n";
+	std::cout << true << '\t' << true << '\t' << (true || true) << '\n';
+	std::cout << false << '\t' << true << '\t' << (false || true) << '\n';
+	std::cout << true << '\t' << false << '\t' << (true || false) << '\n';
+	std::cout << false << '\t' << false << '\t' << (false || false) << '\n';
+
+	std::cout << "\nОператор: &&\n";
+	std::cout << true << '\t' << true << '\t' << (true && true) << '\n';
+	std::cout << false << '\t' << true << '\t' << (false && true) << '\n';
+	std::cout << true << '\t' << false << '\t' << (true && false) << '\n';
+	std::cout << false << '\t' << false << '\t' << (false && false) << '\n';
+	
+	return EXIT_SUCCESS;
+}
