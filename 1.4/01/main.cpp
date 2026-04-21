c63d768048e28b9077a4bbe0c29c5e4a37551174
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-04-21 10:17:23 +0300

    add HW #4

---
+++ 1.4/01/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+	int sum{};
+	int number{};
+
+	do {
+		std::cout << "Введите целое число или число '0', чтобы закончить:" << std::endl;
+		std::cin >> number;
+		sum += number;
+	} while (number != 0);
+
+	std::cout << "Сумма: " << sum << std::endl;
+
+	return EXIT_SUCCESS;
+}
---
+++ 1.4/02/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main() {
+	int sum{};
+	int number{};
+
+	std::cout << "Введите целое число:" << std::endl;
+	std::cin >> number;
+
+	while (number != 0) {
+		sum += number % 10;
+		number /= 10;
+	}
+
+	std::cout << "Сумма цифр: " << sum << std::endl;
+
+	return EXIT_SUCCESS;
+}
