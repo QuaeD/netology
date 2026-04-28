9180e9cb6765e000b2e2997dba44ac2f155c6134
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-04-28 12:37:31 +0300

    add HW #6

---
+++ 1.6/02/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+void powerAndPrint(int value, int power, int result) {
+	for (int i = 0; i < power; i++) {
+		result *= value;
+	}
+
+	std::cout << value << " в степени " << power << " = " << result << std::endl;
+}
+
+int main() {
+	int value = 5;
+	int power = 2; 
+	int result = 1;
+	
+	powerAndPrint(value, power, result);
+
+	value = 3;
+	power = 3;
+	result = 1;
+	
+	powerAndPrint(value, power, result);
+
+	value = 4;
+	power = 4;
+	result = 1;
+
+	powerAndPrint(value, power, result);
+
+	return EXIT_SUCCESS;
+}
