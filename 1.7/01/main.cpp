3a0bee203d424075533393ed9d70c1c76d417163
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-05-06 17:42:15 +0300

    add HW #7

---
+++ 1.7/01/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main() {
+	int a_int{};
+	short a_short{};
+	long a_long{};
+	long long a_long_long{};
+	float a_float{};
+	double a_double{};
+	long double a_long_double{};
+	bool a_bool{};
+
+	std::cout << "short: " << &a_short << " " << sizeof(a_short) << '\n';
+	std::cout << "int: " << &a_int << " " << sizeof(a_int) << '\n';
+	std::cout << "long: " << &a_long << " " << sizeof(a_long) << '\n';
+	std::cout << "long long: " << &a_long_long << " " << sizeof(a_long_long) << '\n';
+	std::cout << "float: " << &a_float << " " << sizeof(a_float) << '\n';
+	std::cout << "double: " << &a_double << " " << sizeof(a_double) << '\n';
+	std::cout << "long double: " << &a_long_double << " " << sizeof(a_long_double) << '\n';
+	std::cout << "bool: " << &a_bool << " " << sizeof(a_bool) << '\n';
+
+	return EXIT_SUCCESS;
+}
---
+++ 1.7/02/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <tuple>
+
+void swap(int& a, int& b) {
+	// через 3 переменную
+	int c = a;
+	a = b;
+	b = c;
+}
+
+void swapArithmetic(int& a, int& b) {
+	// может случиться переполнение, если a и b слишком большие
+	a = a + b;
+	b = a - b;
+	a = a - b;
+}
+
+void swapXor(int& a, int& b) {
+	// a и b должны указывать на разные переменные иначе 0
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+}
+
+void swapTuple(int& a, int& b) {
+	// можно через кортеж, правда в C# это удобнее реализовано
+	std::tie(a, b) = std::make_tuple(b, a);
+}
+
+void swapAlgorithm(int& a, int& b) {
+	// как мне кажется - это самый эффективный вариант
+	std::swap(a, b);
+}
+
+int main() {
+	int a = 5, b = 8;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swap(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swapArithmetic(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swapXor(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swapTuple(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swapAlgorithm(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	return EXIT_SUCCESS;
+}
