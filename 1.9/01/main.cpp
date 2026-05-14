1fac95ede29bdb7ddd89c4b947978c87118cab50
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-05-14 18:15:40 +0300

    add HW #9

---
+++ 1.9/01/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+void print(const int arr[], const int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i];
+		if (i + 1 != size) {
+			std::cout << " ";
+		}
+	}
+	std::cout << '\n';
+}
+
+
+int main() {
+	const int size_1 = 9;
+	int arr_1[size_1] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	print(arr_1, size_1);
+
+	const int size_2 = 4;
+	int arr_2[size_2] = { 6, 5, 4, 8 };
+	print(arr_2, size_2);
+
+	const int size_3 = 5;
+	int arr_3[size_3] = { 1, 4, 3, 7, 5 };
+	print(arr_3, size_3);
+
+	return EXIT_SUCCESS;
+}
---
+++ 1.9/03/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+void reverse(int arr[], const int size) {
+	for (int i = 0; i < size / 2; i++) {
+		std::swap(arr[i], arr[size - 1 - i]);
+	}
+}
+
+void print(const int arr[], const int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i];
+		if (i + 1 != size) {
+			std::cout << " ";
+		}
+	}
+	std::cout << '\n';
+}
+
+int main() {
+	const int size = 9;
+	int arr[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	std::cout << "До функции reverce: ";
+	print(arr, size);
+
+	reverse(arr, size);
+
+	std::cout << "\nПосле функции reverce: ";
+	print(arr, size);
+
+	return EXIT_SUCCESS;
+}
