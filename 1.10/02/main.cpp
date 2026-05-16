cc0b8450c0ab505ee351392725cc9e58d644c494
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-05-16 11:58:17 +0300

    add HW #10

---
+++ 1.10/02/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+double* createArray(int size) {
+	return new double[size] {};
+}
+
+int main() {
+	std::cout << "Введите размер массива: ";
+	int size{};
+	std::cin >> size;
+
+	double* arr_ptr = createArray(size);
+
+	std::cout << "Массив: ";
+
+	for (int i = 0; i < size; i++) {
+		std::cout << arr_ptr[i];
+		if (i + 1 != size) {
+			std::cout << " ";
+		}
+	}
+
+	std::cout << std::endl;
+
+	delete[] arr_ptr;
+
+	return EXIT_SUCCESS;
+}
