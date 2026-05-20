a0a40b0764710030a3843363fc0d9c04edc3c966
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-05-20 15:15:45 +0300

    add HW #11

---
+++ 1.11/01/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	std::cout << "Введите имя: ";
+	std::string name{};
+	std::cin >> name;
+
+	std::cout << "Введите фамилию: ";
+	std::string surname{};
+	std::cin >> surname;
+
+	std::cout << "Здравствуйте, " << name << " " << surname << "!" << std::endl;
+	return EXIT_SUCCESS;
+}
---
+++ 1.11/02/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	std::string answer = "малина";
+	std::string input{};
+
+	while (input != answer) {
+		std::cout << "Угадайте слово: ";
+		std::cin >> input;
+		if (input == answer) {
+			std::cout << "Правильно! Вы победили! Загаданное слово — " << answer << std::endl;
+		}
+		else {
+			std::cout << "Неправильно" << std::endl;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
