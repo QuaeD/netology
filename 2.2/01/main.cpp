e79fb8f68a19c60b7e47740b8930dd99fe0ed7c5
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-06-25 10:32:03 +0300

    Add HW #2.2

---
+++ 2.2/01/main.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+
+enum class Month
+{
+	JANUARY = 1,
+	FEBRUARY,
+	MARCH,
+	APRIL,
+	MAY,
+	JUNE,
+	JULY,
+	AUGUST,
+	SEPTEMBER,
+	OCTOBER,
+	NOVEMBER,
+	DECEMBER,
+};
+
+int main() {
+	int month_num = -1;
+
+	while (month_num != 0) {
+		std::cout << "Введите номер месяца: ";
+		std::cin >> month_num;
+
+		if (month_num == 0) {
+			std::cout << "До свидания" << std::endl;
+			break;
+		}
+
+		if (month_num < 1 || month_num > 12) {
+			std::cout << "Неправильный номер!" << std::endl;
+			continue;
+		}
+
+		Month month = static_cast<Month>(month_num);
+
+		switch (month)
+		{
+		case Month::JANUARY:
+			std::cout << "Январь" << std::endl;
+			break;
+
+		case Month::FEBRUARY:
+			std::cout << "Февраль" << std::endl;
+			break;
+
+		case Month::MARCH:
+			std::cout << "Март" << std::endl;
+			break;
+
+		case Month::APRIL:
+			std::cout << "Апрель" << std::endl;
+			break;
+
+		case Month::MAY:
+			std::cout << "Май" << std::endl;
+			break;
+
+		case Month::JUNE:
+			std::cout << "Июнь" << std::endl;
+			break;
+
+		case Month::JULY:
+			std::cout << "Июль" << std::endl;
+			break;
+
+		case Month::AUGUST:
+			std::cout << "Август" << std::endl;
+			break;
+
+		case Month::SEPTEMBER:
+			std::cout << "Сентябрь" << std::endl;
+			break;
+
+		case Month::OCTOBER:
+			std::cout << "Октябрь" << std::endl;
+			break;
+
+		case Month::NOVEMBER:
+			std::cout << "Ноябрь" << std::endl;
+			break;
+
+		case Month::DECEMBER:
+			std::cout << "Декабрь" << std::endl;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ 2.2/03/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+struct Address
+{
+	std::string town = "";
+	std::string street = "";
+	int house_number = 0;
+	int apartment_number = 0;
+	int postal_code = 0;
+};
+
+void printAddress(Address& address) {
+	std::cout << "Город: " << address.town << std::endl;
+	std::cout << "Улица: " << address.street << std::endl;
+	std::cout << "Номер дома: " << address.house_number << std::endl;
+	std::cout << "Номер квартиры: " << address.apartment_number << std::endl;
+	std::cout << "Индекс: " << address.postal_code << std::endl;
+}
+
+int main() {
+	Address addr_1{
+		.town = "Москва",
+		.street = "Арбат",
+		.house_number = 12,
+		.apartment_number = 8,
+		.postal_code = 123456
+	};
+
+	Address addr_2{
+		.town = "Ижевск",
+		.street = "Пушкина",
+		.house_number = 59,
+		.apartment_number = 143,
+		.postal_code = 953769
+	};
+
+	printAddress(addr_1);
+	std::cout << std::endl;
+	printAddress(addr_2);
+
+	return EXIT_SUCCESS;
+}
