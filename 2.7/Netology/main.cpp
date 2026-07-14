bfcd48a085bdf5e1d58e1a906e89aeb2ea68db41
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-07-14 15:44:48 +0300

    Fix utf8

---
+++ 2.7/Netology/main.cpp
@@ -0,0 +1,34 @@
+﻿#include "preprocessor_directives.h"
+#include "sub_macro.h"
+
+#include <iostream>
+
+#define MODE 1
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int num{};
+
+	do {
+		std::cout << "Введите номер запускаемого задания (1 или 2). Введите 0 для завершения: " << std::endl;
+		std::cin >> num;
+
+		switch (num)
+		{
+		case 1:
+			PreprocessorDirectives hw_1;
+			hw_1.start();
+			break;
+		case 2:
+			SubMacro hw_2;
+			hw_2.start();
+			break;
+		default:
+			break;
+		}
+		std::cout << std::endl;
+	} while (num != 0);
+
+	return EXIT_SUCCESS;
+}
---
+++ 2.7/Netology/sub_macro.h
@@ -0,0 +1,10 @@
+﻿#ifndef SUB_MACRO_H
+#define SUB_MACRO_H
+#endif // !SUB_MACRO_H
+
+class SubMacro
+{
+public:
+	void start();
+};
+
