8d581fbd66054864ab4385241c1c5c929fc8e51b
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-07-18 11:43:23 +0300

    Add HW 2.6

---
+++ CPPM-06/Multi-file_prj_and_lib/03/rhombus.cpp
@@ -0,0 +1,3 @@
+﻿#include "rhombus.h"
+
+Rhombus::Rhombus(int a, int A, int B) : Quadrangle{ a, a, a, a, A, B, A, B, "Ромб" } {}
---
+++ CPPM-06/Multi-file_prj_and_lib/03/rhombus.h
@@ -0,0 +1,12 @@
+﻿#ifndef RHOMBUS_H
+#define RHOMBUS_H
+
+#include "quadrangle.h"
+
+class Rhombus : public Quadrangle
+{
+public:
+	Rhombus(int a, int A, int B);
+};
+
+#endif // !RHOMBUS_H
