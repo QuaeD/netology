85783104b394e2377f9250713f8f7e04619ad679
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-07-18 12:13:26 +0300

    Add HW 2.7

---
+++ CPPM-07/Preproc_and_macr/01/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error "MODE" is not defined!!!
+#endif // !MODE
+
+#if MODE == 1
+#define add(a, b) ((a) + (b))
+#endif
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+#if MODE == 0
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+	std::cout << "Работаю в боевом режиме" << std::endl;
+
+	std::cout << "Введите число 1: ";
+	int num_1{};
+	std::cin >> num_1;
+
+	std::cout << "Введите число 2: ";
+	int num_2{};
+	std::cin >> num_2;
+
+	std::cout << "Результат сложения: " << add(num_1, num_2) << std::endl;
+
+#else
+	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+
+#endif
+
+	return EXIT_SUCCESS;
+}
