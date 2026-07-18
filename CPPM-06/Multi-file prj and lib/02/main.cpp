70a3d6a7ec36a12317baac9a1cec12f2f7d3b2e0
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-07-18 11:41:59 +0300

    Add HW #2.6

---
+++ CPPM-06/Multi-file prj and lib/02/main.cpp
@@ -0,0 +1,44 @@
+﻿#include "counter.h"
+
+#include <iostream>
+#include <string>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	Counter counter{};
+
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+	std::string answer{};
+	std::cin >> answer;
+
+	if (answer == "да" || answer == "д" || answer == "yes" || answer == "y") {
+		std::cout << "Введите начальное значение счётчика: ";
+		int num{};
+		std::cin >> num;
+		counter = Counter(num);
+	}
+
+	bool break_flag = false;
+	do {
+		break_flag = false;
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> answer;
+
+		if (answer == "+") {
+			counter.increase1();
+		}
+		else if (answer == "-") {
+			counter.decrease1();
+		}
+		else if (answer == "=") {
+			std::cout << counter.getValue() << std::endl;
+		}
+		else if (answer == "x" /*eng*/ || answer == "х" /*ru*/) {
+			std::cout << "До свидания!" << std::endl;
+			break_flag = true;
+		}
+	} while (!break_flag);
+
+	return EXIT_SUCCESS;
+}
