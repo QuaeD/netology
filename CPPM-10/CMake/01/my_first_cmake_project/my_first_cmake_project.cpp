3e584b45fd7a894d884bf59578d6939b8702d80f
QuaeD <74988090+QuaeD@users.noreply.github.com>
2026-07-24 16:10:25 +0300

    Add HW #2.10

---
+++ CPPM-10/CMake/01/my_first_cmake_project/my_first_cmake_project.cpp
@@ -0,0 +1,14 @@
+﻿// my_first_cmake_project.cpp: определяет точку входа для приложения.
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Введите имя: ";
+	
+	std::string name{};
+	std::cin >> name;
+
+	std::cout << "Здравствуйте, " << name << "!" << std::endl;
+	return EXIT_SUCCESS;
+}
